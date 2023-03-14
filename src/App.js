28021752f6ecd379a250512c5f2f1e6268a15520
Junghun Kim <junghun1112@gmail.com>
2023-03-14 17:56:54 -0500

    p1: built basic frame

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
 
-import { Container, Toolbar, Typography } from "@mui/material";
+import { AppBar, Container, Toolbar, Typography } from "@mui/material";
 
 function App() {
   return (
-    <Container disableGutters='true'
+    <App disableGutters='true'
     sx={{ 
       marginTop: '80px',
       textAlign: 'center'
     }}>
-      <header>
-        <Toolbar variant="regular"
-        sx={{
-          display:'flex',
-          flexDirection:'column',
-          width: '100%',
-          position: 'fixed',
-          zIndex: '1100',
-          top: '0px',
-          left: 'auto',
-          right: '0px',
-          color: 'rgb(255,255,255)',
-          backgroundColor: 'rgb(46, 59, 85)'
-        }}></Toolbar>
-      </header>
-    </Container>
+      <AppBar
+      sx={{
+        display:'flex',
+        flexDirection:'column',
+        width: '100%',
+        position: 'fixed',
+        zIndex: '1100',
+        top: '0px',
+        left: 'auto',
+        right: '0px',
+        color: 'rgb(255,255,255)',
+        backgroundColor: 'rgb(46, 59, 85)'
 
+      }}>
+        <Container
+        sx={{
+          margin: '0px'
+        }}>
+          <Toolbar variant="regular"
+          sx={{
+            
+          }}></Toolbar>
+        </Container>
+      </AppBar>
+      <Container>
+        <Typography>Hello world</Typography>
+      </Container>
+    </App>
   );
 }
 

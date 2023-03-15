ac567ac8d4485ba9dc03cefe42bdadd46dfebf0d
Junghun Kim <junghun1112@gmail.com>
2023-03-15 18:57:36 -0500

    AppBar Revised

--- src/App.js
+++ src/App.js
@@ -1,42 +1,173 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+// import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+// import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+// import AdbIcon from '@mui/icons-material/Adb';
 
-import { AppBar, Container, Toolbar, Typography } from "@mui/material";
+const pages = ['ABOUT', 'PROJECT'];
+const links = {'ABOUT': '/about', 
+               'PROJECT': '/project'};
+// const settings = ['ABOUT', 'PROJECT'];
 
-function App() {
+function ResponsiveAppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
-    <App disableGutters='true'
-    sx={{ 
-      marginTop: '80px',
-      textAlign: 'center'
-    }}>
-      <AppBar
-      sx={{
-        display:'flex',
-        flexDirection:'column',
-        width: '100%',
-        position: 'fixed',
-        zIndex: '1100',
-        top: '0px',
-        left: 'auto',
-        right: '0px',
-        color: 'rgb(255,255,255)',
-        backgroundColor: 'rgb(46, 59, 85)'
+    <Container maxWidth={false} disableGutters>
+      <AppBar position="static"
+      sx={{bgcolor: 'rgb(46, 59, 85)'}}>
+        <Container maxWidth="xl"
+        sx={{margin: '0px'}}>
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                fontSize: '1.5rem',
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              JEONGHOON KIM
+            </Typography>
 
-      }}>
-        <Container
-        sx={{
-          margin: '0px'
-        }}>
-          <Toolbar variant="regular"
-          sx={{
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem key={page} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center" href='/'>{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href=""
+              sx={{
+                mr: 2,
+                display: { xs: 'flex', md: 'none' },
+                flexGrow: 1,
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              JEONGHOON KIM
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              {pages.map((page) => (
+                <Button 
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  href={links[page]}
+                  sx={{ my: 2, color: 'white', display: 'block', fontSize: '1.2rem' }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+
             
-          }}></Toolbar>
+          </Toolbar>
         </Container>
       </AppBar>
-      <Container>
-        <Typography>Hello world</Typography>
+
+      <Container 
+      sx={{ 
+        marginTop: '80px',
+        textAlign: 'center'
+      }}> Hello World!
+        
       </Container>
-    </App>
+    </Container>
   );
 }
+export default ResponsiveAppBar;
 
-export default App;
+
+
+{/* <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box> */}
